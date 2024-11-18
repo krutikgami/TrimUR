7a71ce55855e0b29c0e45dd59e423917f0845b70
Krutik Gami <krutikgami354@gmail.com>
2024-11-18 11:53:49 +0530

    some changes for env variable load in file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import dotenv from 'dotenv';
+dotenv.config();
 export default defineConfig({
   plugins: [react()],
   server:{
     proxy: {
       '/Api': {
-       target: 'http://localhost:3000',
+       target: process.env.VITE_BACKEND_URL,
        secure: false,
      },
     },
